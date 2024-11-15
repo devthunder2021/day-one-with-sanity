21af074e8746724ae910dcf593be35024b138a29
devthunder2021 <admin@jordanmingle.dev>
2024-11-15 06:06:30 -0500

    feat: bootstrap sanity studio

---
+++ sanity.cli.ts
@@ -0,0 +1,13 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'cb1x8kty',
+    dataset: 'production'
+  },
+  /**
+   * Enable auto-updates for studios.
+   * Learn more at https://www.sanity.io/docs/cli#auto-updates
+   */
+  autoUpdates: true,
+})
