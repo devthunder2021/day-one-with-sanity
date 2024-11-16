5b3d4a40228b5d7fed7f855893b39ff7871cd255
devthunder2021 <admin@jordanmingle.dev>
2024-11-16 12:35:32 -0500

    update studio

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,11 +3,12 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'cb1x8kty',
-    dataset: 'production'
+    dataset: 'production',
   },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
   autoUpdates: true,
+  studioHost: 'garfield',
 })
